dbb6f458675f1842fb5329f51335d7137e063d93
Lily Riopelle <lilykriopelle@gmail.com>
2021-03-05 20:24:32 -0500

    Add styles to NewTopicForm.

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
+import { ALL_ICONS } from "../data/icons";
 
 export default function NewTopicForm() {
   const [name, setName] = useState("");
+  const [icon, setIcon] = useState("");
   const history = useHistory();
 
   const handleSubmit = (e) => {
@@ -18,15 +20,34 @@ export default function NewTopicForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="topic-name">Topic name</label>
-      <input
-        id="topic-name"
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.currentTarget.value)}
-      />
-      <button>Add Topic</button>
-    </form>
+    <section>
+      <form onSubmit={handleSubmit}>
+        <h1 className="center">Create a new topic</h1>
+        <div className="form-section">
+          <input
+            id="topic-name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Topic Name"
+          />
+          <select
+            onChange={(e) => setIcon(e.currentTarget.value)}
+            required
+            defaultValue="default"
+          >
+            <option value="default" disabled hidden>
+              Choose an icon
+            </option>
+            {ALL_ICONS.map(({ name, url }) => (
+              <option key={url} value={url}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button className="center">Add Topic</button>
+      </form>
+    </section>
   );
 }
